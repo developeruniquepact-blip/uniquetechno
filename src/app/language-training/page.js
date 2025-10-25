a97ac3ab7a274a7de1f8efca2887b532e9783c53
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:37:47 +0530

    Update page.js

--- src/app/language-training/page.js
+++ src/app/language-training/page.js
@@ -1,5 +1,3 @@
-import { Metadata } from 'next'
-
 import Footer5 from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import Cta1 from "@/components/sections/cta/Cta1";
@@ -7,12 +5,6 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import LanguagePrimary from "@/components/sections/services/LanguagePrimary";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
-export const metadata = {
-  title: 'Language Training Courses | Technoglobe Institute in Sharjah, UAE',
-  description: 'Improve your communication and career prospects with Technoglobe’s language training courses in Sharjah, UAE. Learn English, Arabic, and other languages with expert instructors for professional and personal growth.',
-  keywords: ['language courses Sharjah', 'English training UAE', 'Arabic classes', 'professional language skills', 'communication courses', 'Technoglobe institute', 'career development UAE']
-}
-
 export default function Services() {
     return (
         <div>
