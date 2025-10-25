66cad724d96d41df0c4aade0f1124504311aca46
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 13:59:33 +0530

    Update page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,9 +1,17 @@
+import { Metadata } from 'next'
+
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import BlogMain from "@/components/layout/main/BlogMain";
 import Cta1 from "@/components/sections/cta/Cta1";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
+export const metadata = {
+  title: 'Blogs | Technoglobe IT Training & Development Institute in Sharjah, UAE',
+  description: 'Explore the latest blogs from Technoglobe, Sharjah’s leading IT training institute in the UAE. Get insights on technology trends, career tips, and professional development in the IT industry.',
+  keywords: ['Technoglobe blogs', 'IT training UAE', 'technology trends', 'career development', 'IT institute Sharjah', 'tech education UAE', 'professional training']
+}
+
 
 export default function Blogs() {
 	return (
