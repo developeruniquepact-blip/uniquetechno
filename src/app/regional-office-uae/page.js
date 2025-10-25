6c85830642628eb2ac0e75c766ad7a39c1ecc53c
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:06:34 +0530

    Update page.js

--- src/app/regional-office-uae/page.js
+++ src/app/regional-office-uae/page.js
@@ -1,3 +1,5 @@
+import { Metadata } from 'next'
+
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import Cta1 from "@/components/sections/cta/Cta1";
@@ -5,6 +7,12 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import Team2 from "@/components/sections/teams/Team2";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
+export const metadata = {
+  title: 'Regional Office UAE | Technoglobe IT Training Institute in UAE',
+  description: 'Visit Technoglobe’s regional office in the UAE. Learn about our locations, contact details, and how we support students and professionals with IT training and career development across the region.',
+  keywords: ['Technoglobe regional office', 'IT training UAE', 'office locations Sharjah', 'contact Technoglobe', 'training institute UAE', 'career support', 'Technoglobe UAE']
+}
+
 export default function Team() {
 	return (
 		<div>
