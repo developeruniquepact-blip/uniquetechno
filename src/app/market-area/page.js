e4ef88a032b860c921f1c76855fa9f4a2a147339
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:37:23 +0530

    Update page.js

--- src/app/market-area/page.js
+++ src/app/market-area/page.js
@@ -1,5 +1,3 @@
-import { Metadata } from 'next'
-
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 //import Careers1 from "@/components/sections/careers/Careers1";
@@ -8,12 +6,6 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
 import Link from "next/link"; // Make sure to import Link
-export const metadata = {
-  title: 'Market Area | Technoglobe IT Training Institute in Sharjah, UAE',
-  description: 'Explore Technoglobe’s market area in Sharjah, UAE. Discover our reach, training centers, and the communities we serve in IT education and professional development across the region.',
-  keywords: ['Technoglobe market area', 'IT training Sharjah', 'training centers UAE', 'professional development UAE', 'education outreach', 'Technoglobe institute', 'IT education Sharjah']
-}
-
 
 export default function Careers() {
 	return (
