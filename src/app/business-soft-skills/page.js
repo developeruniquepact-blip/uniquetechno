b49d4381b88e53b0f34c624786cb6502c6554486
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:41:53 +0530

    Update page.js

--- src/app/business-soft-skills/page.js
+++ src/app/business-soft-skills/page.js
@@ -1,5 +1,3 @@
-import { Metadata } from 'next'
-
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import Cta1 from "@/components/sections/cta/Cta1";
@@ -7,12 +5,6 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import BskPrimary from "@/components/sections/services/BskPrimary";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
-export const metadata = {
-  title: 'Business and Soft Skills Courses | Technoglobe Training Institute in Sharjah, UAE',
-  description: 'Enhance your career with Technoglobe’s Business and Soft Skills courses in Sharjah, UAE. Develop leadership, communication, and professional skills to succeed in today’s competitive workplace.',
-  keywords: ['business courses Sharjah', 'soft skills training UAE', 'professional development', 'leadership training', 'communication skills', 'Technoglobe institute', 'career growth UAE']
-}
-
 export default function Services() {
     return (
         <div>
