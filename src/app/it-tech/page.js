19e65770cc0bce309fd6c120577981f08c2d39b6
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:38:44 +0530

    Update page.js

--- src/app/it-tech/page.js
+++ src/app/it-tech/page.js
@@ -1,5 +1,3 @@
-import { Metadata } from 'next'
-
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import Cta1 from "@/components/sections/cta/Cta1";
@@ -7,12 +5,6 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import ItTechPrimary from "@/components/sections/services/ItTechPrimary";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
-export const metadata = {
-  title: 'IT & Tech Courses | Technoglobe Training Institute in Sharjah, UAE',
-  description: 'Advance your career with Technoglobe’s IT & Tech courses in Sharjah, UAE. Learn programming, networking, cybersecurity, and the latest technology skills from industry experts.',
-  keywords: ['IT courses Sharjah', 'Tech training UAE', 'programming courses', 'networking courses', 'cybersecurity training', 'Technoglobe IT institute', 'career development UAE']
-}
-
 export default function Services() {
     return (
         <div>
