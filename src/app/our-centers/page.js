2e44151ea99c147ee0b0a3f1b04ba92af1c73519
Uniquepact <developer.uniquepact@gmail.com>
2025-10-25 14:04:52 +0530

    Update page.js

--- src/app/our-centers/page.js
+++ src/app/our-centers/page.js
@@ -1,3 +1,5 @@
+import { Metadata } from 'next'
+
 import Footer from "@/components/layout/footer/Footer5";
 import Header from "@/components/layout/header/Header";
 import Cta1 from "@/components/sections/cta/Cta1";
@@ -5,6 +7,12 @@ import HeroInner from "@/components/sections/heros/HeroInner";
 import PortfoliosPrimary from "@/components/sections/portfolios/PortfoliosPrimary";
 import TjMagicCursor from "@/components/shared/others/TjMagicCursor";
 import ClientWrapper from "@/components/shared/wrappers/ClientWrapper";
+export const metadata = {
+  title: 'Our Centers | Technoglobe IT Training Institute in Sharjah, UAE',
+  description: 'Discover Technoglobe’s training centers across Sharjah and the UAE. Explore our state-of-the-art facilities, hands-on IT labs, and expert-led classrooms designed for effective learning and skill development.',
+  keywords: ['Technoglobe centers', 'IT training Sharjah', 'training facilities UAE', 'IT institute Sharjah', 'hands-on IT labs', 'professional development', 'Technoglobe UAE']
+}
+
 export default function Portfolios() {
 	return (
 		<div>
