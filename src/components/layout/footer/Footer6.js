a920f4814d64799fcba575214e8397068800f802
Uniquepact <developer.uniquepact@gmail.com>
2025-10-27 17:09:56 +0530

    Update Footer6.js

--- src/components/layout/footer/Footer6.js
+++ src/components/layout/footer/Footer6.js
@@ -1,7 +1,6 @@
 import ButtonPrimary from "@/components/shared/buttons/ButtonPrimary";
 import FormSelect from "@/components/shared/Inputs/FormSelect";
 import Link from "next/link";
-import N8nChatbot from "./N8nnChatbot";
 
 const Footer6 = () => {
 	return (
